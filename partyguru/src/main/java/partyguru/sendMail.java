d896100b493af70aab924dceb185d543a3e88d63
Bastian <bastian@harren.co>
2015-06-26 09:46:23 +0200

    Maven Benutzung eingefügt.

--- partyguru/src/main/java/partyguru/sendMail.java
+++ partyguru/src/main/java/partyguru/sendMail.java
@@ -2,32 +2,31 @@ package partyguru;
 
 
 
-	import java.util.Properties;
+import java.util.Properties;
 
-	import javax.mail.Message;
+import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-	 
 
-	/**
-	 * Klasse die dem Versenden von Emails über einen SMTP Server dient.
-	 * Übergeben werden müssen die String Felder: an email Adresse, Titel der Nachricht, Nachricht in Plain text.
-	 * Die Funktion gibt bei Erfolg eine 1 zurück, bei einem Fehler eine 0
-	 * @author siegmunf
-	 *
-	 */
 
-	public class sendMail {
-	 
-		
+/**
+ * Klasse die dem Versenden von Emails über einen SMTP Server dient.
+ * Übergeben werden müssen die String Felder: an email Adresse, Titel der Nachricht, Nachricht in Plain text.
+ * Die Funktion gibt bei Erfolg eine 1 zurück, bei einem Fehler eine 0
+ * @author siegmunf
+ *
+ */
+
+public class sendMail {
+
+
 
 	public static int emailSenden(String an, String titel, String nachricht){
-		
+
 		final String username = "derpartyguru@gmail.com";
 		final String password = "reichwald";
 		int erfolg = 1;
@@ -39,29 +38,29 @@ import javax.mail.internet.MimeMessage;
 		props.put("mail.smtp.port", "587");
 
 		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
+				new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
-		  });
+		});
 
 		try {
 
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress("derpartyguru@gmail.com"));
 			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse(an));
+					InternetAddress.parse(an));
 			message.setSubject(titel);
 			message.setText(nachricht);
-			
-			
+
+
 			Transport.send(message);
 
-			
+
 
 		} catch (MessagingException e) {
 			erfolg = 0;
-						
+
 		} 
 		return erfolg;
 	}
@@ -76,7 +75,7 @@ import javax.mail.internet.MimeMessage;
 
 
 
-	}
+}
 
 
 
