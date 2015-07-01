b34cfcd4330bb2cdd1ecbe97d8e8fa38061b9d21
Lukas <Lukas@Lukas-Laptop>
2015-07-01 17:52:00 +0200

    Change name of JDatePicker.

--- partyguru/src/main/java/GUI_Date/CalenderFunction.java
+++ partyguru/src/main/java/GUI_Date/CalenderFunction.java
@@ -2,21 +2,11 @@ package GUI_Date;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Properties;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
-
-import javax.swing.SwingUtilities;
-
-import net.sourceforge.jdatepicker.DateModel;
 import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
 import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
 import net.sourceforge.jdatepicker.impl.UtilDateModel;
@@ -27,27 +17,27 @@ import net.sourceforge.jdatepicker.impl.UtilDateModel;
  * @version 1.0 beta
  */
 
-public class JDatePicker_new extends JDatePickerImpl
+public class CalenderFunction extends JDatePickerImpl
 {
 	private static final long serialVersionUID = 1L;
 
 	UtilDateModel mModel;
 	
-	public JDatePicker_new(JDatePanelImpl panel, UtilDateModel model) 
+	public CalenderFunction(JDatePanelImpl panel, UtilDateModel model) 
 	{
 		super(panel);	
 		
 		mModel = model;
 	}
 	
-	public static JDatePicker_new createDatePicker()
+	public static CalenderFunction createDatePicker()
 	{
 		UtilDateModel model = new UtilDateModel();
 		model.setDate(model.getYear(), model.getMonth(), model.getDay());
 		model.setSelected(true);	
 		
 		JDatePanelImpl datePanel = new JDatePanelImpl(model);
-		JDatePicker_new picker = new JDatePicker_new(datePanel, model);
+		CalenderFunction picker = new CalenderFunction(datePanel, model);
 		
 		return picker;
 	}
@@ -57,6 +47,7 @@ public class JDatePicker_new extends JDatePickerImpl
 		return mModel.getValue();
 	}
 
+	//Only for testing
 	public static void main(String[] args) 
 	{
 		JFrame frame = new JFrame("DatePicker Test");
@@ -64,7 +55,7 @@ public class JDatePicker_new extends JDatePickerImpl
 		JPanel panel = new JPanel();
 		frame.add(panel);
 
-		JDatePicker_new  datePicker = createDatePicker();
+		CalenderFunction  datePicker = createDatePicker();
 		panel.add(datePicker);
 		
 		JButton button = new JButton("senden");
